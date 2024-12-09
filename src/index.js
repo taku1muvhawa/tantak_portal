226afce742f5ed8aedd527d3b3eb1b1b259e2e36
taku1muvhawa <taku1muvhawa@gmail.com>
2024-12-09 07:51:00 +0200

    8-12-2024 commit 6

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
